83bd28c8e15246651ab6781905d8b6033d9e27e2
TheMitchWorksPro <TheMitchWorksPro@gmail.com>
2016-11-23 15:25:27 -0500

    Edits made to all files (content identified as text version at top of each file)
    These files were tweaked at least a little as part of this text file version of the content:
            modified:   .gitignore
            modified:   GitHub_win7/GitHub_Intro_OtherUsefulCommands.txt
            modified:   GitHub_win7/GitHub_Basic_Commands_and_ScratchNotes.txt
            modified:   GitHub_win7/GitHub_Intro_GetInfo_Commands.txt
            modified:   GitHub_win7/GitHub_Lesson1_SetupProject.txt
            modified:   GitHub_win7/GitHub_Lesson2_InitializeExistingCodeProject.txt
            modified:   GitHub_win7/GitHub_Lesson3_BranchingCommands.txt
            modified:   GitHub_win7/GitHub_Lesson4_UndoingMistakes.txt
            modified:   GitHub_win7/GitHub_Setup-Advanced_Work-w-Multiple-User-Accounts.txt
            modified:   GitHub_win7/sample_bash_scripts/Update-Online-Repo.sh

--- GitHub_win7/sample_bash_scripts/Update-Online-Repo.sh
+++ GitHub_win7/sample_bash_scripts/Update-Online-Repo.sh
@@ -2,7 +2,7 @@
 # this script will add and commit all changes
 # it also pushes all changes up to the online repo
 # use single quotes to pass in the commit comment string as first argument
-# Example:  Update-Online-Repo 'Replaced $ w/ - \$ throughout text file content'
+# Example:  ./Update-Online-Repo.sh 'Replaced $ w/ - \$ throughout text file content'
 
 git add --all
 git status
