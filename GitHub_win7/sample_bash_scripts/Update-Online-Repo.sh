cd326cb23488a6dc760a2ec3e14f51608eaad071
TheMitchWorksPro <TheMitchWorksPro@gmail.com>
2016-11-23 14:09:10 -0500

    minor content updates to .txt content files

---
+++ GitHub_win7/sample_bash_scripts/Update-Online-Repo.sh
@@ -0,0 +1,9 @@
+# simple bash script
+# this script will add and commit all changes
+# it also pushes all changes up to the online repo
+# use single quotes to pass in the commit comment string as first argument
+
+git add --all
+git status
+git commit -m "$*"
+git push
