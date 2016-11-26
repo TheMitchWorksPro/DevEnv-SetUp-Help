585e5426d7ceb04a72457dc085c7698153a1fae6
TheMitchWorksPro <TheMitchWorksPro@gmail.com>
2016-11-26 17:55:17 -0500

    updates to branching ppt file, sample bash scripts including example of how-to-use it line, and Thumbs.db

--- GitHub_win7/sample_bash_scripts/Commit-Local-Repo.sh
+++ GitHub_win7/sample_bash_scripts/Commit-Local-Repo.sh
@@ -1,6 +1,6 @@
 # simple bash script
-# this script will add and commit all changes
-# it also pushes all changes up to the online repo
+# this script will add and commit all changes to local working project
+# it uses status and log commands to show what is happening along the way
 # use single quotes to pass in the commit comment string as first argument
 # Execution Syntax:  ./<name-of-this-script> '<your comments for the commit operation'
 
@@ -8,4 +8,3 @@ git add --all
 git status
 git commit -m "$*"
 git log -n 1
-git push
